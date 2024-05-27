4c3d620c95506ac7a516244d1d7e7285517a4876
Daniel Marques Coutinho <163597645+DanielMCoutinho@users.noreply.github.com>
2024-05-27 12:22:09 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+## Conversor de moedas
+#### Conversor de moedas para o real brasileiro (BRL)
+Esse projeto faz o consumo da API de cotações de moedas Awesome API, ou seja, o valor é sempre o mais atual possível. Neste projeto as tecnologias utilizadas foram:
+- ReactJS
+- API
+- Bootstrap e FontAwesome
+- Um pouquinho de CSS
+
+O resultado pode ser observado [nesse link](https://conversor-moeda.vercel.app/).
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+
+import Conversor from './components/Conversor'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Conversor de moedas</h1>
+        <div className="conversor-style">
+          <Conversor moedaA="USD" moedaB="BRL" />
+          <Conversor moedaA="CAD" moedaB="BRL" />
+          <Conversor moedaA="EUR" moedaB="BRL" />
+          <Conversor moedaA="GBP" moedaB="BRL" />
+        </div>
+    </div>
+  )
+}
+
+export default App
